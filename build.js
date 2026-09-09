00969bd1bb8777c3bbb9edd7684c3e824c2116de
sayFLUMMOX <140507968+FlummoxGamer@users.noreply.github.com>
2026-09-09 07:33:09 +0530

    Disable GM_getValue and GM_setValue grants
    Commented out GM_getValue and GM_setValue grants.

--- build.js
+++ build.js
@@ -16,6 +16,8 @@ build({
 // @match        https://discord.com/app
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=discord.com
 // @grant        unsafeWindow
+// @grant        GM_getValue
+// @grant        GM_setValue
 // ==/UserScript==`
   }
 }).catch(() => process.exit(1));
